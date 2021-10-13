b44a1ba93cd7775672bb02c4186eadde8b72c749
eshasinghai <92069157+eshasinghai@users.noreply.github.com>
2021-10-13 08:16:59 -0400

    Update IntroScreen.java

--- src/core/IntroScreen.java
+++ src/core/IntroScreen.java
@@ -1,12 +1,15 @@
-// added a comment
-
 package core;
+
+import java.awt.Font;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+import org.newdawn.slick.font.effects.GradientEffect;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -15,36 +18,52 @@ public class IntroScreen extends BasicGameState
 	int id;
 	
 	public boolean forward;
-	
-	public boolean menu;
-	
 	public boolean instruc;
-	
-	
 	
 	IntroScreen(int id) 
 	{
 		this.id = id;
 	}
 
-	
+//	public class ImageResize {
+//	
+//	public void main(String[] args)
+//	{
+//		ImageIcon title = new ImageIcon("res/introTitle.png"); 
+//		Image dabImage = dabIcon.getImage(); 
+//		Image modifiedDabImage = dabImage.getScaledInstance(600, 600, java.awt.Image.SCALE_SMOOTH); 
+//		dabIcon = new ImageIcon (modifiedDabImage); 
+//	}
+//}
+
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException 
 	{
 		// This code happens when you enter a game state for the *first time.*
 		gc.setShowFPS(true);
 		forward = false;
 		instruc = false;
-		menu = false;
 	}
 
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException 
 	{
-		// Sets background to the specified RGB color
-		g.setBackground(new Color(0, 200, 200));
-		g.drawString("Press 'N' to Start!", 400, 500);
 		
-
+//		Sets background to the specified RGB color
+        g.setBackground(new Color(2, 2, 10));
+//		g.drawString("Press Space to Start!", 400, 500);
+		
+		Image title = new Image("res/introTitle.png");  
+		g.drawImage(title, Engine.RESOLUTION_X/8, (int)((Engine.RESOLUTION_Y)/7.2));
+		
+		Image font = new Image("res/introFont.png");  
+		g.drawImage(font, (int)((Engine.RESOLUTION_X)/3), (int)((Engine.RESOLUTION_Y)/1.2));
+		
 	}
+	
+//	public void GradientEffect(java.awt.Color topColor, java.awt.Color bottomColor, float scale)
+//	{
+//		
+//		
+//	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException
 	{	
@@ -59,11 +78,6 @@ public class IntroScreen extends BasicGameState
 			instruc = false;
 		}
 		
-		if (menu) {
-			sbg.enterState(3);
-			menu = false;
-		}
-		
 	}
 
 	public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException 
@@ -71,7 +85,6 @@ public class IntroScreen extends BasicGameState
 		// This code happens when you enter a gameState.  
 		forward = false;
 		instruc = false;
-		menu = false;
 	}
 
 	public void leave(GameContainer gc, StateBasedGame sbg) 
@@ -81,22 +94,15 @@ public class IntroScreen extends BasicGameState
 	
 	public void keyPressed(int key, char c)
 	{
-//		if (key == Input.KEY_SPACE) {
-//			forward = true;
-//		}
+		if (key == Input.KEY_SPACE) {
+			forward = true;
+		}
 		
 		if (key == Input.KEY_I) {
 			instruc = true;
 		}
 		
-		if (key == Input.KEY_N) {
-			menu = true;
-		}
-		
 	}
-
-
-	
 	
 	// Returns the ID code for this game state
 	public int getID() 
