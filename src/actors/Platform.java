09adb4f73962f3b83ec73410009e283c67bf2b8a
atcupps <91998663+atcupps@users.noreply.github.com>
2021-10-20 09:02:39 -0400

    Update Platform.java

--- src/actors/Platform.java
+++ src/actors/Platform.java
@@ -3,6 +3,8 @@ package actors;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
+import core.Game;
+
 public class Platform {
 
 	protected float x, y, w, h;
@@ -21,5 +23,48 @@ public class Platform {
 		//THIS IS TEMPORARY JUST FOR TESTING, REPLACE WITH ACTUAL GRAPHICS LATER
 		g.setColor(color);
 		g.fillRect(x, y, w, h);
+	}
+	
+	public void changeY(float playerYSpeed) {
+		y -= playerYSpeed;
+	}
+	
+	public void setChangeY(float amount) {
+		y += amount;
+	}
+	
+	public boolean collidesDown(float yp) {
+		if (yp > y && yp < y + h) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean collidesUp(float y1) {
+		return false;
+	}
+	
+	public boolean collidesLeft(float x1) {
+		return false;
+	}
+	
+	public boolean collidesRight(float x2) {
+		return false;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public float getH() {
+		return h;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getW() {
+		return w;
 	}
 }
