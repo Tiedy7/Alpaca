8e568ff5e5eab1fcb086dddd31d04d6013e6310b
Tiedy7 <92095486+Tiedy7@users.noreply.github.com>
2021-10-28 09:00:59 -0400

    Update Player.java

--- src/actors/Player.java
+++ src/actors/Player.java
@@ -3,6 +3,7 @@ package actors;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 
 
