2fbfecea7f51c2a556ca03ab8d54b5bc8b484c50
FlareJ <92417584+FlareJ@users.noreply.github.com>
2021-11-19 08:55:14 -0500

    Update Projectile.java

--- src/actors/Projectile.java
+++ src/actors/Projectile.java
@@ -27,30 +27,30 @@ public class Projectile {
 	private SpriteSheet projectile = null;
 	
 	private float ax, vx, ay, vy; //acceleration & velocity
+	private float vxmax, vymax; //max velocity
+	private float permvx, permvy; //velocity
+	private float targetx, targety; //target
 	
-	public Projectile(float x, float y, int sw, int sh, float vx, float vy, int damage, boolean breaksOnWall) {
+	public Projectile(float targetx, float targety, float x, float y) {
 		this.x = x;
 		this.y = y;
-		this.w = Game.function.scaleX(sw);
-		this.h = Game.function.scaleY(sh);
-		this.vx = vx;
-		this.vy = vy;
-		this.damage = damage;
-		this.breaksOnWall = breaksOnWall;
 	}
 	
 	public void render(Graphics g, float difX, float difY) {
 		//IMPORTANT DON'T DELETE, ITS JUST FOR THE SUBCLASS, NOT THIS
-		
+		/*
 		setImage("res/Enemy Sprites/droneEnemy.png");
 		projectile.setFilter(Image.FILTER_NEAREST);
 		projectile.startUse();
 		projectile.getSubImage(walkLoop, 0).drawEmbedded(x + difX, y + difY, w, h);
 		projectile.endUse();
-		
+		*/
 	}
 
 	public void update() {
+		vx = permvx;
+		vy = permvy;
+		
 		vx += ax;
 		vy += ay;
 		
@@ -82,41 +82,7 @@ public class Projectile {
 	}
 	
 	protected void checkCollisions(ArrayList<Platform> platforms) {
-		//This is all useless code I think (for projectiles). Don't delete it though, I'm not sure
-		
-		float tempX = x + vx;
-		float tempY = y + vy;
-		for (Platform p : platforms) {
-			if (vx > 0) {
-				if (p.collidesRight(tempX,tempY,w,h)) {
-					vx = 0;
-					tempX = p.getX() - w;
-				}
-			}
-			if (vx < 0) {
-				if (p.collidesLeft(tempX, tempY, w, h)) {
-					vx = 0;
-					tempX = p.getX() + p.getW();
-				}
-			}
-			if (vy > 0) {
-				if (p.collidesDown(tempX, tempY, w, h)) {
-					vy = 0;
-					tempY = p.getY() - h;
-					Game.playerTouchesPlatform();
-				}
-			}
-			if (vy < 0) {
-				if (p.collidesUp(tempX, tempY, w, h)) {
-					vy = 0;
-					tempY = p.getY() + p.getH();
-					ay = 1;
-				}
-			}
-		}
-		x = tempX;
-		y = tempY;
-		
+		//This is all useless code (for projectiles)
 	}
 	
 	public void contactDamage(Player p, float x, float y, float w, float h) {
@@ -153,5 +119,10 @@ public class Projectile {
 			System.out.println("Image not found!");
 		}
 		
+	}
+	
+	public int getTime()
+	{
+		return time;
 	}
 }
