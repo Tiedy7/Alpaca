ea3e9f5383e8c462f9f73355cde3d50eebbf4596
atcupps <91998663+atcupps@users.noreply.github.com>
2021-11-20 13:53:42 -0500

    Update DroneEnemy.java

--- src/actors/DroneEnemy.java
+++ src/actors/DroneEnemy.java
@@ -1,7 +1,5 @@
 package actors;
 
-import java.util.ArrayList;
-
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -14,8 +12,6 @@ import core.Game;
 import actors.Player;
 
 public class DroneEnemy extends Enemy {
-
-	private float x, w, y, h;
 	
 	private int walkLoop, walkRowNum;
 	private boolean walkRow;
@@ -34,17 +30,15 @@ public class DroneEnemy extends Enemy {
 
 	
 	public DroneEnemy(float sx, float sy) {
-		this.w = Game.function.scaleX(64);
-		this.h = Game.function.scaleY(64);
-		this.x = sx;
-		this.y = sy;
+		w = Game.function.scaleX(64);
+		h = Game.function.scaleY(64);
+		x = sx;
+		y = sy;
 		
-		this.ay = Game.function.scaleY(1);
-		this.vy = 0;
+		ay = Game.function.scaleY(1);
+		vy = 0;
 		vx = 0;
 		ax = 0;
-		
-		damage = 1;
 		
 		walkRow = false;
 		walkRowNum = 0;
@@ -55,12 +49,10 @@ public class DroneEnemy extends Enemy {
 		faceRight = true;
 		faceLeft = false;
 		isJump = false;
+		isEnemy = true;
 	}
 	
 	public void render(Graphics g, float difX, float difY) {
-		//THIS IS TEMPORARY JUST FOR TESTING, REPLACE WITH ACTUAL GRAPHICS LATER
-//		g.setColor(color);
-//		g.fillRect(Engine.RESOLUTION_X / 2 - (w / 2),(2 * Engine.RESOLUTION_Y / 3) - h,w,h);
 		
 		setImage("res/Enemy Sprites/droneEnemy.png");
 		droneEnemy.setFilter(Image.FILTER_NEAREST);
@@ -69,22 +61,11 @@ public class DroneEnemy extends Enemy {
 		droneEnemy.endUse();
 	}
 	
-	public void update() {
-		if (Game.playerX > x + (w*0.1)) vx = 2;
-		else if (Game.playerX + Game.playerW < x + (w*0.9)) vx = -2;
-		else vx = 0;
-		vx += ax;
-		vy += ay;
-		
-		if (Game.playerY > y + w) vy = 2;
-		else if (Game.playerY + Game.playerW < y) vy = -2;
-		else vy = 0;
-		vx += ax;
-		vy += ay;
-		
+	public void update() {		
 		
 		if (time % 12 == 0) {
-			walkLoop++;	
+			walkLoop++;
+			
 		}
 		if (walkLoop >= 8) {
 			walkLoop = 0;
@@ -99,48 +80,6 @@ public class DroneEnemy extends Enemy {
 		if (!walkRow) {
 			walkRowNum = 0;
 		}
-		
-		//COLLISIONS
-		checkCollisions(Game.platforms);
-		contactDamage(Game.player, x, y, w, h);
-		
-		//System.out.println("actualx:" + this.x);
-		//System.out.println("actualy:" + this.y);
-	}
-	
-	protected void checkCollisions(ArrayList<Platform> platforms) {
-		float tempX = x + vx;
-		float tempY = y + vy;
-		boolean canFall = false;
-		for (Platform p : platforms) {
-			if (vx > 0) {
-				if (p.collidesRight(tempX,tempY,w,h)) {
-					vx = 0;
-					tempX = p.getX() - w;
-				}
-			}
-			if (vx < 0) {
-				if (p.collidesLeft(tempX, tempY, w, h)) {
-					vx = 0;
-					tempX = p.getX() + p.getW();
-				}
-			}
-			if (vy > 0) {
-				if (p.collidesDown(tempX, tempY, w, h)) {
-					vy = 0;
-					tempY = p.getY() - h;
-				}
-			}
-			if (vy < 0) {
-				if (p.collidesUp(tempX, tempY, w, h)) {
-					vy = 0;
-					tempY = p.getY() + p.getH();
-					ay = 1;
-				}
-			}
-		}
-		x = tempX;
-		y = tempY;
 	}
 	
 	public void collidesDown(float newY) {
