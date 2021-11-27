c97e577508b97f130e5546b1660c83adcf9f3fdd
atcupps <91998663+atcupps@users.noreply.github.com>
2021-11-27 14:14:34 -0500

    Update Level.java

--- src/core/Level.java
+++ src/core/Level.java
@@ -24,21 +24,21 @@ public class Level {
 		actors = new ArrayList<Actor>();
 		pickups = new ArrayList<Pickup>();
 		switch (level) {
-		case 0:	//TUTORIAL LEVEL
-				platforms.add(new Platform(16,7,5,5));		
-				platforms.add(new Platform(3,12,24,3));
-				platforms.add(new Platform(28,8,23,3));
+		case 0:	//TUTORIAL LEVEL ??
+				platforms.add(new Platform(16,7,5,5,2));		
+				platforms.add(new Platform(3,12,24,3,1));
+				platforms.add(new Platform(28,8,23,3,0));
 				actors.add(new GroundEnemy(function.scaleX(300), function.scaleY(400)));
 				actors.add(new DroneEnemy(function.scaleX(300), function.scaleY(100)));
-				pickups.add(new Pickup(9,11,1,1,new Color(250,250,0),"doubleJump"));
+				pickups.add(new Pickup(9,11,new Color(250,250,0),"doubleJump"));
 				break;
 		case 1: //LEVEL 1?
-				platforms.add(new Platform(3,12,24,3));
-				platforms.add(new Platform(6,8,5,5));
-				platforms.add(new Platform(28,-8,2,16));
-				pickups.add(new Pickup(8,6,1,1,new Color(125,225,0),"wallJump"));
-				pickups.add(new Pickup(19,11,1,1,new Color(0,250,0),"heal"));
-				pickups.add(new Pickup(8,4,1,1,new Color(0,0,250),"dash"));
+				platforms.add(new Platform(3,12,24,3,1));
+				platforms.add(new Platform(6,7,5,5,0));
+				platforms.add(new Platform(28,-8,2,16,1));
+				pickups.add(new Pickup(8,6,new Color(125,225,0),"wallJump"));
+				pickups.add(new Pickup(19,11,new Color(0,250,0),"heal"));
+				pickups.add(new Pickup(8,4,new Color(0,0,250),"dash"));
 				actors.add(new DwayneBoss(function.scaleX(1500),function.scaleY(200)));
 				break;
 		}
