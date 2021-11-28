81fa2f9afe1908dcaf2a18a89a67c37b5aee9897
atcupps <91998663+atcupps@users.noreply.github.com>
2021-11-28 15:08:00 -0500

    Update Level.java

--- src/core/Level.java
+++ src/core/Level.java
@@ -1,9 +1,9 @@
 package core;
 
 import java.util.ArrayList;
-import java.util.function.Function;
 
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 
 import actors.Actor;
 import actors.DroneEnemy;
@@ -14,10 +14,12 @@ import actors.Platform;
 
 public class Level {
 
-	ArrayList<Platform> platforms;
-	ArrayList<Actor> actors;
-	ArrayList<Pickup> pickups;
-	Functions function = Game.function;
+	private ArrayList<Platform> platforms;
+	private ArrayList<Actor> actors;
+	private ArrayList<Pickup> pickups;
+	private Functions function = Game.function;
+	
+	private float levelW, levelH, miniXOffset, miniYOffset;
 	
 	public Level(int level) {
 		platforms = new ArrayList<Platform>();
@@ -31,6 +33,10 @@ public class Level {
 				actors.add(new GroundEnemy(function.scaleX(300), function.scaleY(400)));
 				actors.add(new DroneEnemy(function.scaleX(300), function.scaleY(100)));
 				pickups.add(new Pickup(9,11,new Color(250,250,0),"doubleJump"));
+				levelW = 3392;
+				levelH = 1908;
+				miniXOffset = 0;
+				miniYOffset = 500;
 				break;
 		case 1: //LEVEL 1?
 				platforms.add(new Platform(3,12,24,3,1));
@@ -40,8 +46,35 @@ public class Level {
 				pickups.add(new Pickup(19,11,new Color(0,250,0),"heal"));
 				pickups.add(new Pickup(8,4,new Color(0,0,250),"dash"));
 				actors.add(new DwayneBoss(function.scaleX(1500),function.scaleY(200)));
+				levelW = 2958;
+				levelH = 1664;
+				miniXOffset = 256;
+				miniYOffset = 640;
 				break;
 		}
+	}
+	
+	public void minimapRender(Graphics g) {
+		//MINIMAP BACKGROUDN
+		g.setColor(new Color(0,0,0,150));
+		g.fillRect(Game.function.scaleX(1920 - 414), Game.function.scaleY(30), 384, 216);
+		
+		//RENDER PLATFORMS ONTO MINIMAP
+		g.setColor(new Color(100,100,100,150));
+		for (Platform p : platforms) {
+			g.fillRect(Game.function.scaleX(Game.function.minimapScaleX(p.getX() + miniXOffset, 384, levelW)) + Game.function.scaleX(1920 - 414), Game.function.scaleY(Game.function.minimapScaleY(p.getY() + miniYOffset, 216, levelH)) + Game.function.scaleY(30), Game.function.scaleX(Game.function.minimapScaleX(p.getW(), 384, levelW)), Game.function.scaleY(Game.function.minimapScaleY(p.getH(), 216, levelH)));
+		}
+		
+		//RENDERS PLAYER ONTO MINIMAP
+		g.setColor(new Color(104, 161, 252));
+		g.fillRect(Game.function.scaleX(Game.function.minimapScaleX(Game.player.getX() + miniXOffset, 384, levelW)) + Game.function.scaleX(1920 - 414), Game.function.scaleY(Game.function.minimapScaleY(Game.player.getY() + miniYOffset, 216, levelH)) + Game.function.scaleY(30), Game.function.scaleX(Game.function.minimapScaleX(Game.player.getW(), 384, levelW)), Game.function.scaleY(Game.function.minimapScaleY(Game.player.getH(), 216, levelH)));
+		
+		
+		
+		
+		//MINIMAP BORDER OUTLINE
+		g.setColor(new Color(255,255,255));
+		g.drawRect(Game.function.scaleX(1920 - 414), Game.function.scaleY(30), 384, 216);
 	}
 	
 	public int getPlatformsSize() {
