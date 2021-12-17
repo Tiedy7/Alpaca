8acc926cc34eec0ead50a9fb1ea58d5577390738
Tiedy7 <92095486+Tiedy7@users.noreply.github.com>
2021-12-17 08:46:02 -0500

    Update Level.java

--- src/core/Level.java
+++ src/core/Level.java
@@ -17,13 +17,13 @@ public class Level {
 
 	private ArrayList<Platform> platforms;
 	private ArrayList<Actor> actors;
-	private ArrayList<Pickup> pickups;
+	private static ArrayList<Pickup> pickups;
 	private Functions function = Game.function;
 	
 	static int curLevel;
 	private float levelW, levelH, miniXOffset, miniYOffset;
 	
-	private int minX, minY, maxX, maxY;
+	public static int minX, minY, maxX, maxY;
 	
 	public static boolean[][] tiles;
 	
@@ -34,9 +34,9 @@ public class Level {
 		pickups = new ArrayList<Pickup>();
 		int basic;
 		switch (level) {
-		case 0:	//TUTORIAL LEVEL ??
+		case 000:	//TUTORIAL LEVEL ??
 			basic = 3;
-			curLevel = 0;
+			curLevel = 000;
 			//floor
 			platforms.add(new Platform(10, 12, 51, 30, basic));
 			platforms.add(new Platform(26, 10, 6, 2, basic));
@@ -47,6 +47,9 @@ public class Level {
 			//actors.add(new GroundEnemy((27*function.scaleX(64)), (10*function.scaleX(64))));
 			actors.add(new GoombaEnemy((24*function.scaleX(64)), (10*function.scaleY(64)), (20*function.scaleX(64)), (25*function.scaleY(64)), true));
 			//levelW = 2048;
+			//pickups.add(new Pickup(function.scaleX(384), function.scaleY(512), wallJump));
+//			pickups.add(new Pickup(11, 8, new Color(250,250,0),"wallJump"));
+//			pickups.add(new Pickup(14, 8, new Color(250,250,0),"dash"));
 			levelW = 3904;
 			levelH = 3328;
 			miniYOffset = 640;
@@ -54,74 +57,130 @@ public class Level {
 			break;
 			
 		case 1:	//Abyss Climb
-				basic = 3;
-				curLevel = 1;
-				//climbing out
-				platforms.add(new Platform(0, 40, 32, 16, basic));
-				platforms.add(new Platform(4, 37, 4, 3, basic));
-				platforms.add(new Platform(12, 34, 4, 1, basic));
-				platforms.add(new Platform(17, 31, 4, 1, basic));
-				platforms.add(new Platform(10, 28, 4, 1, basic));
-				platforms.add(new Platform(4, 26, 4, 2, basic));
-				platforms.add(new Platform(11, 24, 4, 1, basic));
-				platforms.add(new Platform(17, 23, 4, 1, basic));
-				platforms.add(new Platform(24, 22, 4, 2, basic));
-				platforms.add(new Platform(16, 19, 4, 1, basic));
-				platforms.add(new Platform(12, 16, 4, 1, basic));
-				platforms.add(new Platform(18, 13, 3, 1, basic));
-				platforms.add(new Platform(21, 10, 7, 4, basic));
-				platforms.add(new Platform(4, 10, 12, 1, basic));
-				platforms.add(new Platform(14, 8, 2, 2, basic));
-				platforms.add(new Platform(4, 6, 6, 4, basic));
-				//wall
-				platforms.add(new Platform(0, -10, 4, 50, basic));
-				platforms.add(new Platform(28, -10, 4, 50, basic));
-				//ceiling
-				platforms.add(new Platform(4, -10, 2, 12, basic));
-				platforms.add(new Platform(9, -10, 19, 12, basic));
-				//enemy
-				actors.add(new GoombaEnemy((17*function.scaleX(64)), (21*function.scaleY(64)), (17*function.scaleX(64)), (20*function.scaleY(64)), false));
+		case 100:	//Abyss Climb
+			basic = 3;
+			curLevel = 100;
+			//climbing out
+			platforms.add(new Platform(0, 40, 32, 16, basic));
+			platforms.add(new Platform(4, 37, 4, 3, basic));
+			platforms.add(new Platform(12, 34, 4, 1, basic));
+			platforms.add(new Platform(17, 31, 4, 1, basic));
+			platforms.add(new Platform(10, 28, 4, 1, basic));
+			platforms.add(new Platform(4, 26, 4, 2, basic));
+			platforms.add(new Platform(11, 24, 4, 1, basic));
+			platforms.add(new Platform(17, 23, 4, 1, basic));
+			platforms.add(new Platform(24, 22, 4, 2, basic));
+			platforms.add(new Platform(16, 19, 4, 1, basic));
+			platforms.add(new Platform(12, 16, 4, 1, basic));
+			platforms.add(new Platform(18, 13, 3, 1, basic));
+			platforms.add(new Platform(21, 10, 7, 4, basic));
+			platforms.add(new Platform(4, 10, 12, 1, basic));
+			platforms.add(new Platform(14, 8, 2, 2, basic));
+			platforms.add(new Platform(4, 6, 6, 4, basic));
+			//wall
+			platforms.add(new Platform(0, -10, 4, 50, basic));
+			platforms.add(new Platform(28, -10, 4, 50, basic));
+			//ceiling
+			platforms.add(new Platform(4, -10, 2, 12, basic));
+			platforms.add(new Platform(9, -10, 19, 12, basic));
+			//enemy
+			actors.add(new GoombaEnemy((22*function.scaleX(64)), (8*function.scaleY(64)), (21*function.scaleX(64)), (27*function.scaleY(64)), false));	
+			levelW = 7396;
+			levelH = 4160;
+			miniXOffset = 2674;
+			miniYOffset = 640;
+			break;
+		case 101: //Abyss Climb 2
+			basic = 3;
+			curLevel = 101;
+			//climbing out
+			platforms.add(new Platform(4, 40, 2, 16, basic));
+			platforms.add(new Platform(9, 40, 19, 16, basic));
 				
-				levelW = 7396;
-				levelH = 4160;
-				miniXOffset = 2674;
-				miniYOffset = 640;
-				break;
-		case 2: //Abyss Climb 2
-				basic = 3;
-				curLevel = 2;
-				//climbing out
-				platforms.add(new Platform(4, 40, 2, 16, basic));
-				platforms.add(new Platform(9, 40, 19, 16, basic));
+			platforms.add(new Platform(13, 37, 4, 3, basic));
+			platforms.add(new Platform(17, 34, 11, 6, basic));
+			platforms.add(new Platform(26, 31, 2, 3, basic));
+			platforms.add(new Platform(21, 28, 3, 1, basic));
+			platforms.add(new Platform(26, 25, 2, 1, basic));
+			platforms.add(new Platform(10, 22, 13, 2, basic));
+			platforms.add(new Platform(4, 19, 2, 5, basic));
+			platforms.add(new Platform(10, 16, 14, 1, basic));
+			platforms.add(new Platform(26, 13, 2, 2, basic));
+			platforms.add(new Platform(10, 10, 14, 1, basic));
+			platforms.add(new Platform(13, 8, 8, 2, basic));
+			platforms.add(new Platform(15, 6, 4, 2, basic));
 				
-				platforms.add(new Platform(13, 37, 4, 3, basic));
-				platforms.add(new Platform(17, 34, 11, 6, basic));
-				platforms.add(new Platform(26, 31, 2, 3, basic));
-				platforms.add(new Platform(21, 28, 3, 1, basic));
-				platforms.add(new Platform(26, 25, 2, 1, basic));
-				platforms.add(new Platform(10, 22, 13, 2, basic));
-				platforms.add(new Platform(4, 19, 2, 5, basic));
-				platforms.add(new Platform(10, 16, 14, 1, basic));
-				platforms.add(new Platform(26, 13, 2, 2, basic));
-				platforms.add(new Platform(10, 10, 14, 1, basic));
-				platforms.add(new Platform(13, 8, 8, 2, basic));
-				platforms.add(new Platform(15, 6, 4, 2, basic));
-				
-				//wall
-				platforms.add(new Platform(0, -10, 4, 66, basic));
-				platforms.add(new Platform(28, -10, 4, 66, basic));
-				//ceiling
-				platforms.add(new Platform(4, -10, 11, 12, basic));
-				platforms.add(new Platform(19, -10, 9, 12, basic));
-				//enemy
-				actors.add(new GoombaEnemy((17*function.scaleX(64)), (14*function.scaleY(64)), (10*function.scaleX(64)), (23*function.scaleY(64)), false));
-				//actors.add(new GroundEnemy((16*function.scaleX(64)), (4*function.scaleY(64))));
-				
-				levelW = 7509;
-				levelH = 4224;
-				miniXOffset = 7509 / 2 - 2048 / 2;
-				miniYOffset = 608;
-				break;
+			//wall
+			platforms.add(new Platform(0, -10, 4, 66, basic));
+			platforms.add(new Platform(28, -10, 4, 66, basic));
+			//ceiling
+			platforms.add(new Platform(4, -10, 11, 12, basic));
+			platforms.add(new Platform(18, -10, 10, 12, basic));
+			//enemy
+			actors.add(new GoombaEnemy((17*function.scaleX(64)), (14*function.scaleY(64)), (10*function.scaleX(64)), (23*function.scaleY(64)), false));
+			//actors.add(new GroundEnemy((16*function.scaleX(64)), (4*function.scaleY(64))));
+
+			levelW = 7509;
+			levelH = 4224;
+			miniXOffset = 7509 / 2 - 2048 / 2;
+			miniYOffset = 608;
+			break;
+		case 102: //Crash Zone Surface
+			basic = 5;
+			curLevel = 102;
+			platforms.add(new Platform(-16, 40, 28, 16, basic));
+			platforms.add(new Platform(15, 40, 53, 16, basic));
+			platforms.add(new Platform(21, 37, 47, 3, basic));
+			platforms.add(new Platform(27, 34, 41, 3, basic));
+			platforms.add(new Platform(33, 31, 35, 3, basic));
+
+			//other room
+			platforms.add(new Platform(-9, 37, 3, 3, 3));
+			platforms.add(new Platform(-13, 38, 2, 2, 3));
+			platforms.add(new Platform(-16, 35, 5, 3, 3));
+			platforms.add(new Platform(-16, 26, 5, 3, 3));
+			platforms.add(new Platform(-16, 23, 2, 3, 3));
+
+			levelW = 7509;
+			levelH = 4224;
+			miniXOffset = 7509 / 2 - 2048 / 2;
+			miniYOffset = 608;
+			break;
+		case 103: //Crash Zone Ship Entrance
+			basic = 3;
+			curLevel = 103;
+			platforms.add(new Platform(0, 40, 61, 16, 5));
+			platforms.add(new Platform(0, 35, 38, 3, basic));
+			platforms.add(new Platform(40, 37, 3, 3, basic));
+			platforms.add(new Platform(36, 38, 2, 2, basic));
+			platforms.add(new Platform(24, 38, 2, 2, basic));
+			platforms.add(new Platform(12, 38, 2, 2, basic));
+			platforms.add(new Platform(0, 38, 2, 2, basic));
+			platforms.add(new Platform(0, 26, 38, 3, basic));
+			platforms.add(new Platform(0, 23, 35, 3, basic));
+			platforms.add(new Platform(0, 18, 20, 5, basic));
+			platforms.add(new Platform(0, 5, 8, 13, basic));
+			platforms.add(new Platform(11, 5, 9, 10, basic));
+
+			//other room
+			platforms.add(new Platform(64, 40, 4, 16, 2));
+
+			levelW = 7509;
+			levelH = 4224;
+			miniXOffset = 7509 / 2 - 2048 / 2;
+			miniYOffset = 608;
+			break;
+		case 300: //Waterfall Crash Zone Entrance
+			basic = 5;
+			curLevel = 300;
+			platforms.add(new Platform(-15, 31, 31, 25, basic));
+			platforms.add(new Platform(16, 0, 2, 56, 0));
+
+			levelW = 7509;
+			levelH = 4224;
+			miniXOffset = 7509 / 2 - 2048 / 2;
+			miniYOffset = 608;
+			break;
 		}
 	}
 	
@@ -175,5 +234,10 @@ public class Level {
 	
 	public Pickup getPickup(int i) {
 		return pickups.get(i);
+	}
+	
+	public static void generateCoin(float x, float y) {
+		pickups.add(new Pickup((int) x / 64, (int) y / 64, "coin"));
+		Game.pickups.add(pickups.get(pickups.size() - 1));
 	}
 }
